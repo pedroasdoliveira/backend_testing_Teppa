1e6a913877738dbd7bf6720b0f8d2983742743f7
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-08-01 13:20:49 -0300

    feat(index): import the routes

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cors())
-app.use('/blog', router.register, router.allUsers)
+app.use('/blog', router.register, router.allUsers, router.userId, router.editUser, router.deleteUser)
 
 const port = 3333 || process.env.PORT;
 
