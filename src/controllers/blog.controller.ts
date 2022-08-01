bf22f13d0bc5b61bce239d08e50c9648bd4727b8
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-08-01 13:20:03 -0300

    feat(blogController): implement a CRUD for blog users

--- src/controllers/blog.controller.ts
+++ src/controllers/blog.controller.ts
@@ -46,7 +46,60 @@ const showUsers = async (req: Request, res: Response) => {
   }
 };
 
+const UserId = async (req: Request, res: Response) => {
+  try {
+    const userRef = db.collection("users").doc(req.params.id);
+    const response = await userRef.get();
+    res.status(200).send(response.data());
+  } catch (error: any) {
+    res.status(404).send({ message: "Error" + error });
+  }
+};
+
+const editUser = async (req: Request, res: Response) => {
+  try {
+    const userJson = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      age: req.body.age,
+      email: req.body.email,
+      profession: req.body.profession,
+      password: req.body.password,
+      confirmPassword: req.body.confirmPassword,
+    };
+
+    if (userJson.password !== userJson.confirmPassword) {
+      res.status(402).send({ message: "Senhas incompativeis!" });
+      return;
+    }
+
+    const userRef = await db
+      .collection("users")
+      .doc(req.params.id)
+      .update(userJson);
+    res.status(201).send(userRef);
+  } catch (error: any) {
+    res.status(404).send({ message: "Error" + error });
+  }
+};
+
+const deleteUser = async (req: Request, res: Response) => {
+  try {
+    const response = await db.collection('users').doc(req.params.id).delete();
+    res.status(204).send({
+      message: 'Usuário deletado!',
+      response,
+    })
+  }
+  catch (error: any) {
+    res.status(404).send({ message: "Error" + error });
+  }
+}
+
 export = {
   registerUser,
   showUsers,
+  UserId,
+  editUser,
+  deleteUser,
 };
