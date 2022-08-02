693fb93c56714ac37a5618174f2e9724ce2e1fd6
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-08-02 00:52:56 -0300

    feat(userTypes): implement types for User in Blog

---
+++ src/dto/user.types.ts
@@ -0,0 +1,9 @@
+export type UserInfosTypes = {
+    firstName: string;
+    lastName: string;
+    age: number;
+    email: string;
+    profession: string;
+    password: string;
+    confirmPassword: string;
+}
