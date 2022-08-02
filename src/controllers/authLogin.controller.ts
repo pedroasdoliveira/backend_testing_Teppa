5077bd0b698a5517a8cb0d5005442a43cd6c8395
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-08-02 00:50:08 -0300

    fix(authLogin): Get email user with req.body to generate a token

--- src/controllers/authLogin.controller.ts
+++ src/controllers/authLogin.controller.ts
@@ -1,44 +1,67 @@
 import { compare } from "bcrypt";
 import { Request, Response } from "express";
+import { collection, query, where, getDocs } from "firebase/firestore";
+import { UserLogin } from '../dto/login.types'
 import { sign } from "jsonwebtoken";
-import * as admin from "firebase-admin";
-import * as credentials from "../db/key.json";
-
-admin.initializeApp({
-  credential: admin.credential.cert(credentials as admin.ServiceAccount),
-});
-
-const db = admin.firestore();
-
+import { db } from '../db/credentials'
 class AuthLoginController {
   async execute(req: Request, res: Response) {
     try {
-      const { email, password } = req.body;
+      const emailRef = db.collection('users')
+      const snapshot = await emailRef.where('email', "==", req.body.email).get()
 
-      const userEmail = db.collection("users").doc(email);
-      const response = await userEmail.get();
-
-      const user = response.data();
-
-      if (!user) {
-        return res.status(404).send({ message: "Email or password incorrect" });
+      if (snapshot.empty) {
+        res.status(404).send({message: 'Error aplication'})
       }
 
-      const passwordMatch = await compare(password, user.password);
-
-      if (!passwordMatch) {
-        return res.status(404).send({ message: "Email or password incorrect" });
-      }
-
-      const token = sign({}, "chave_secreta", {
-        subject: email,
-        expiresIn: "1d",
+      snapshot.forEach(doc => {
+        
       });
-      return token;
-    } catch (error: any) {
+    }
+    catch (error: any) {
       res.status(404).send({ message: "Error" + error });
     }
   }
+
+  // async execute(req: Request, res: Response) {
+  //   try {
+  //     const { email, password }: UserLogin = req.body;
+
+  //     const userEmail = db.collection("users").doc('KxE8r6sTxhDfZ4kRVsdM');
+  //     const response = await userEmail.get();
+
+  //     const user = response.data();
+  //     console.log(user)
+
+  //     if (!user) {
+  //       return res.status(404).send({ message: "Email or password incorrect!" });
+  //     }
+
+  //     const passwordMatch = await compare(password, user.password);
+
+  //     if (!passwordMatch) {
+  //       return res.status(404).send({ message: "Email or password incorrect" });
+  //     }
+
+  //     const token = sign({}, "chave_secreta", {
+  //       subject: email,
+  //       expiresIn: "1d",
+  //     });
+
+  //     delete user.password;
+
+  //     res.status(200).send({
+  //       token,
+  //       user
+  //     })
+  //     return {
+  //       token,
+  //       user
+  //     };
+  //   } catch (error: any) {
+  //     res.status(404).send({ message: "Error" + error });
+  //   }
+  // }
 }
 
 export { AuthLoginController };
