bd32218db0f4eec4edb38cde533e23a423c3bcb9
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-08-02 15:00:21 -0300

    feat(authLogin): remove console.log and comments

--- src/controllers/authLogin.controller.ts
+++ src/controllers/authLogin.controller.ts
@@ -18,14 +18,10 @@ class AuthLoginController {
       snapshot.forEach(doc => {
         user.push(doc.data())
       });
-      console.log(user)
       
       if (!user) {
         return res.status(404).send({ message: "Email or password incorrect!" });
       }
-
-      console.log(req.body.password)
-      console.log(user.password)
 
       const passwordMatch = await compare(req.body.password, user[0].password);
 
@@ -38,11 +34,6 @@ class AuthLoginController {
         expiresIn: "1d",
       });
 
-      console.log({
-        token,
-        user
-      })
-
       res.status(200).send({
         token,
         user
@@ -54,46 +45,6 @@ class AuthLoginController {
       res.status(404).send({ message: "Error" + error });
     }
   }
-
-  // async execute(req: Request, res: Response) {
-  //   try {
-  //     const { email, password }: UserLogin = req.body;
-
-  //     const userEmail = db.collection("users").doc('KxE8r6sTxhDfZ4kRVsdM');
-  //     const response = await userEmail.get();
-
-  //     const user = response.data();
-  //     console.log(user)
-
-  //     if (!user) {
-  //       return res.status(404).send({ message: "Email or password incorrect!" });
-  //     }
-
-  //     const passwordMatch = await compare(password, user.password);
-
-  //     if (!passwordMatch) {
-  //       return res.status(404).send({ message: "Email or password incorrect" });
-  //     }
-
-  //     const token = sign({}, "chave_secreta", {
-  //       subject: email,
-  //       expiresIn: "1d",
-  //     });
-
-  //     delete user.password;
-
-  //     res.status(200).send({
-  //       token,
-  //       user
-  //     })
-  //     return {
-  //       token,
-  //       user
-  //     };
-  //   } catch (error: any) {
-  //     res.status(404).send({ message: "Error" + error });
-  //   }
-  // }
 }
 
 export { AuthLoginController };
